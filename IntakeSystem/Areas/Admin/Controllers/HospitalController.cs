57c44fafa62d80a64cdf7f3a0cc24e1a506f1b46
yanal <yanal.fard@gmail.com>
2021-04-17 15:14:00 +0430

    list added

---
+++ IntakeSystem/Areas/Admin/Controllers/HospitalController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Services.Services;
+
+namespace IntakeSystem.Areas.Admin.Controllers
+{
+    public class HospitalController : Controller
+    {
+        private Core _core;
+
+        public HospitalController(Core core)
+        {
+            _core = core;
+        }
+
+        // GET: Admin/Hospital
+        public ActionResult Index()
+        {
+            
+            return View("List");
+        }
+    }
+}
