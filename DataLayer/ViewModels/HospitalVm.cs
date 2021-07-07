736e253b50abb656de0c430cbf06540904053041
Mehdi Sahandi <mehdisahandi@gmail.com>
2021-07-07 15:59:45 +0430

    update

--- DataLayer/ViewModels/HospitalVm.cs
+++ DataLayer/ViewModels/HospitalVm.cs
@@ -16,31 +16,37 @@ namespace DataLayer.ViewModels
         [MaxLength(50, ErrorMessage = "تعداد کاراکتر بیشتر است")]
         [StringLength(50)]
         public string Name { get; set; }
-        public int LocationId { get; set; }
-        [Display(Name = "نام ")]
+        [Display(Name = "شهر ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        public int? LocationId { get; set; }
+        [Display(Name = "آدرس ")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         [MaxLength(500, ErrorMessage = "تعداد کاراکتر بیشتر است")]
         [StringLength(500)]
         public string Address { get; set; }
+        [Display(Name = "نقشه ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
         public string Lat { get; set; }
         public string Lon { get; set; }
         [MaxLength(11, ErrorMessage = "تعداد کاراکتر بیشتر است")]
         [StringLength(11)]
         [Display(Name = " تلفن 1")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
-        [RegularExpression("[0]{1}[9]{1}[0-9]{9}", ErrorMessage = "شماره تلفن وارد شده معتبر نمی باشد")]
+       // [RegularExpression("[0]{1}[9]{1}[0-9]{9}", ErrorMessage = "شماره تلفن وارد شده معتبر نمی باشد")]
         public string TellNo { get; set; }
         [MaxLength(11, ErrorMessage = "تعداد کاراکتر بیشتر است")]
         [StringLength(11)]
         [Display(Name = "تلفن 2")]
-        [RegularExpression("[0]{1}[9]{1}[0-9]{9}", ErrorMessage = "شماره تلفن وارد شده معتبر نمی باشد")]
+      //  [RegularExpression("[0]{1}[9]{1}[0-9]{9}", ErrorMessage = "شماره تلفن وارد شده معتبر نمی باشد")]
         public string TellNo2 { get; set; }
-        public int AdminId { get; set; }
+        //public int AdminId { get; set; }
         public string ImageUrl { get; set; }
         public int CatagoryId { get; set; }
         public List<TblCatagory> TblCatagory { get; set; }
         public string AboutUs { get; set; }
         public short Fee { get; set; }
-        public bool IsActive { get; set; }
+        //public bool IsActive { get; set; }
+
+        public List<TblLocation> TblLocations { get; set; }
     }
 }
--- IntakeSystem/Areas/Admin/Controllers/HospitalController.cs
+++ IntakeSystem/Areas/Admin/Controllers/HospitalController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,7 +13,12 @@ namespace IntakeSystem.Areas.Admin.Controllers
     public class HospitalController : Controller
     {
         private Core _core = new Core();
-
+        TblUser SelectUser()
+        {
+            int userId = Convert.ToInt32(User.Identity.Name);
+            TblUser selectUser = _core.User.GetById(userId);
+            return selectUser;
+        }
         public ActionResult Index(int PageId = 1, string Result = "", int InPageCount = 20)
         {
             List<TblHospital> hospitals = _core.Hospital.Get().ToList();
@@ -22,8 +28,67 @@ namespace IntakeSystem.Areas.Admin.Controllers
         {
             return View(new HospitalVm()
             {
-                TblCatagory=_core.Catagory.Get().ToList(),
+                TblCatagory = _core.Catagory.Get().ToList(),
+                TblLocations = _core.Location.Get(i => i.LocationParentId == null).ToList()
             });
+        }
+        [HttpPost]
+        public ActionResult PtCreate(HospitalVm hospitalVm, HttpPostedFileBase imgUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imgUrl == null)
+                {
+                    ModelState.AddModelError("ImageUrl", "عکس آپلود کنید");
+                }
+                else if (!imgUrl.IsImage())
+                {
+                    ModelState.AddModelError("ImageUrl", "عکس مورد نظر مشکل دارد ");
+                }
+                else if (imgUrl.ContentLength > 10485760)
+                {
+                    ModelState.AddModelError("ImageUrl", "سایز عکس بیشتر است");
+                }
+                else
+                {
+                    #region Addimage
+                    hospitalVm.ImageUrl = Guid.NewGuid() + Path.GetExtension(imgUrl.FileName);
+                    imgUrl.SaveAs(Server.MapPath("/Resources/Images/Hospital/" + hospitalVm.ImageUrl));
+                    ImageResizer img = new ImageResizer();
+                    img.Resize(Server.MapPath("/Resources/Images/Hospital/" + hospitalVm.ImageUrl),
+                        Server.MapPath("/Resources/Images/Hospital/Thumb/" + hospitalVm.ImageUrl));
+                    #endregion
+
+                    TblHospital addHospital = new TblHospital();
+                    addHospital.AboutUs = hospitalVm.AboutUs;
+                    addHospital.Address = hospitalVm.Address;
+                    addHospital.CatagoryId = hospitalVm.CatagoryId;
+                    addHospital.AdminId =SelectUser().UserId;
+                    addHospital.DateCreated = DateTime.Now;
+                    addHospital.Fee = hospitalVm.Fee;
+                    addHospital.ImageUrl = hospitalVm.ImageUrl;
+                    addHospital.IsActive = true;
+                    addHospital.Lat = hospitalVm.Lat;
+                    addHospital.Lon = hospitalVm.Lon;
+                    addHospital.LocationId = (int)hospitalVm.LocationId;
+                    addHospital.Name = hospitalVm.Name;
+                    _core.Hospital.Add(addHospital);
+                    _core.Save();
+                    return RedirectToAction("Index");
+                }
+            }
+            hospitalVm.TblCatagory = _core.Catagory.Get().ToList();
+            hospitalVm.TblLocations = _core.Location.Get(i => i.LocationParentId == null).ToList();
+            return View(hospitalVm);
+        }
+        public ActionResult ShowCity(int id = 0)
+        {
+            List<TblLocation> list = new List<TblLocation>();
+            if (id != 0)
+            {
+                list = _core.Location.Get(i => i.LocationParentId == id).ToList();
+            }
+            return PartialView(list);
         }
         public ActionResult PtEdit()
         {
